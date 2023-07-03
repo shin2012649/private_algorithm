04a81a2e3587c47f0861957ef47446f53869477e
shin2012649 <129751169+shin2012649@users.noreply.github.com>
2023-07-03 13:51:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1864 ms, Memory: 128500 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 표의 크기
+        int M = Integer.parseInt(st.nextToken()); // 합을 구해야 하는 횟수
+
+        int[][] table = new int[N + 1][N + 1]; // 표의 배열 (인덱스 1부터 시작)
+
+        // 표 입력
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                table[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 누적 합 계산
+        int[][] prefixSum = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + table[i][j];
+            }
+        }
+
+        // 구간 합 계산
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+            System.out.println(sum);
+        }
+    }
+}
